656baabed8e4675809a19119da13263b1e3d381d
YuraMalinko <malinkowork@gmail.com>
2022-12-04 22:31:21 +0300

    Made negative tests for Table class properties

---
+++ BookingSystem.Test/TablesTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace BookingSystem.Test
+{
+    public class TablesTest
+    {
+        [TestCase(0)]
+        [TestCase(-5)]
+        public void NumberTableTest_WhenNumberTableIncorrect_ShouldThrowArgumentOutOfRangeException(int numberTable)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Table(numberTable, 2));
+        }
+
+        [TestCase(0)]
+        [TestCase(-5)]
+        public void NumberOfSeatz_WhenNumberOfSeatsIncorrect_ShouldThrowArgumentOutOfRangeException(int numberSeats)
+        {
+            Assert.Throws<ArgumentOutOfRangeException> (() => new Table(2,numberSeats));
+        }
+    }
+}
