3b0d0685961b0ba9fc6e48fc63b2488577fb8775
YuraMalinko <malinkowork@gmail.com>
2022-12-04 21:49:38 +0300

    changes in code style

--- BookingSystem/Reservation.cs
+++ BookingSystem/Reservation.cs
@@ -8,11 +8,10 @@
 
         public string TelephoneNumberGuest { get; private set; }
 
-        public DateTime DateReservation { get; private set; } = new DateTime();
-
         public int NumberOfGuest { get; private set; }
 
-        public int NumberTable { get; set; }
+        public int NumberTable { get; private set; }
+        public DateTime DateReservation { get; private set; } = new DateTime();
 
         public Reservation(int numberTable,int numberReservation, string nameGuest, string telephoneNumberGuest, DateTime dateReservation, int numberOfGuest)
         {
@@ -24,19 +23,14 @@
             TelephoneNumberGuest = telephoneNumberGuest;
         }
 
-        public void ShowFullInfoReservation()
-        {
-            Console.WriteLine($"Бронь номер : \t{NumberReservation}");
-            Console.WriteLine($"Cтол номер : \t{NumberTable}");
-            Console.WriteLine($"Дата брони: \t{DateReservation:D}");
-            Console.WriteLine($"На имя : \t{NameGuest}");
-            Console.WriteLine($"Телефон : \t{TelephoneNumberGuest}");
-            Console.WriteLine($"Колличество гостей : {NumberOfGuest} ");
-        }
-
         public override string ToString()
         {
-            return $"Reservation #{NumberReservation} Date {DateReservation:D} ";
+            return $"Reservation #{NumberReservation}" +
+                   $"\tDate {DateReservation}" +
+                   $"\tNumber of guest : {NumberOfGuest}" +
+                   $"\tTable #{NumberTable}" +
+                   $"\tName guest : {NameGuest} " +
+                   $"\tTelephone number : {TelephoneNumberGuest}";
         }
 
         public override bool Equals(object? obj)
